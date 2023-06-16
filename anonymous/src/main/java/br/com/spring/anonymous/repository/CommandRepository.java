daec2294b68d7f61ceaae1a4d9d771201174b980
Segato <Fabio.Segato@firstdata.com>
2023-06-16 17:06:37 -0300

    command

--- anonymous/src/main/java/br/com/spring/anonymous/repository/CommandRepository.java
+++ anonymous/src/main/java/br/com/spring/anonymous/repository/CommandRepository.java
@@ -11,7 +11,7 @@ import br.com.spring.anonymous.entity.Command;
 @Repository
 public interface CommandRepository extends JpaRepository<Command, String> {
 
-	@Query(value = "select t.id, t.cmd_Group,t.cmd_Order,t.dsc,t.command,t.aj_Type,t.atb_01,t.atb_02,t.atb_03,t.atb_04,t.atb_05,t.status "
+	@Query(value = "select t.id, t.cmd_Group,t.cmd_Order,t.dsc,t.command,t.aj_Type,t.atb_01,t.atb_02,t.atb_03,t.atb_04,t.atb_05,t.status,'0' as hash "
 			+ "from CTR_AJ_COMMANDS t "
 			+ "where (:id IS NULL or t.id like %:id%) and (:command IS NULL or t.command like %:command%) "
 			+ "and rownum <= 10", nativeQuery = true)
