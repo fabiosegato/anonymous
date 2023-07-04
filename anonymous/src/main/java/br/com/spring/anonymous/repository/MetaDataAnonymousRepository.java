7c31738e5b5e135fb1b47b145bf5965f4d5ee749
Segato <Fabio.Segato@firstdata.com>
2023-07-04 11:28:01 -0300

    schedule dto

--- anonymous/src/main/java/br/com/spring/anonymous/repository/MetaDataAnonymousRepository.java
+++ anonymous/src/main/java/br/com/spring/anonymous/repository/MetaDataAnonymousRepository.java
@@ -10,12 +10,6 @@ import br.com.spring.anonymous.entity.MetaDataAnonymous;
 @Repository
 public interface MetaDataAnonymousRepository extends JpaRepository<MetaDataAnonymous, Long> { 
 	
-	//@Query("select distinct(t.taskType) from MetaDataAnonymous t order by t.taskType")
-	//List<String> carregaTypes();
-
-	//@Query("select t from MetaDataAnonymous t where t.taskType = :taskType")
-	//List<MetaDataAnonymous> carregaSettings(@Param("taskType") String p_taskType);
-	
 	List<MetaDataAnonymous> findBytaskType(String taskType);
 
 }
