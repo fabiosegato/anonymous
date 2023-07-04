b4100d8f64baa0b0e71ab5680ab6f93eec8190e5
Segato <Fabio.Segato@firstdata.com>
2023-07-04 11:28:12 -0300

    schedule dto

--- anonymous/src/main/java/br/com/spring/anonymous/repository/ScheduleRepository.java
+++ anonymous/src/main/java/br/com/spring/anonymous/repository/ScheduleRepository.java
@@ -1,24 +1,12 @@
 package br.com.spring.anonymous.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import br.com.spring.anonymous.entity.Schedule;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, String> {
 
-	@Query(value="select t.SCHEDULE_ID,t.TASK_GROUP,t.SCHEDULE_TYPE,t.SCHEDULE_VALUE,t.VALID_FROM"
-			+ ",t.VALID_TO,t.STATUS,AGENT,t.LAST_SCHEDULE,t.INSTANCE,t.CRITICAL_JOB,t.NUMBER_ATTEMPTS,t.PRMT_1 "
-			+ "from CTR_AJ_SCHEDULE t "
-			+ "where (:taskGroup IS NULL or t.TASK_GROUP like %:taskGroup%) "
-			+ "and (:instance IS NULL or t.INSTANCE like %:instance%) "
-			+ "and rownum <= 20",nativeQuery = true)
-	List<Schedule> carregaSchedule(String taskGroup,String instance);
-	
 
 }
 
